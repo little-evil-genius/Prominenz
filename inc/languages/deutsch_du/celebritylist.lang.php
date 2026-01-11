7dae6900cbb2064847f01ad9410b3224e4cdc5ea
Lara <75126217+little-evil-genius@users.noreply.github.com>
2026-01-11 10:48:23 +0100

    Create celebritylist.lang.php

---
+++ inc/languages/deutsch_du/celebritylist.lang.php
@@ -0,0 +1,71 @@
+<?php
+$l['celebritylist_banner'] = "Es {1} {2} neue {3} überprüft werden.";
+$l['celebritylist_banner_anon_single'] = "muss";
+$l['celebritylist_banner_anon_plural'] = "müssen";
+$l['celebritylist_banner_count_single'] = "eine";
+$l['celebritylist_banner_person_single'] = "Berühmheit";
+$l['celebritylist_banner_person_plural'] = "Berühmheiten";
+
+$l['celebritylist_lists'] = "Listen";
+$l['celebritylist_main'] = "Übersicht aller Berühmheiten";
+$l['celebritylist_desc'] = "Hier findet ihr die Übersicht über berühmte Charaktere in unserem Forum. Um deinen Charakter hinzuzufügen, musst nur das folgende Formular ausfüllen. Das Team wird dann dein Eintrag überprüfen und entsprechend annehmen oder ablehnen.<br>Bei Fragen oder Probleme kannst du dich immer ans Team wenden.";
+$l['celebritylist_count'] = "{1} {2}";
+$l['celebritylist_count_single'] = "Berühmheit";
+$l['celebritylist_count_plural'] = "Berühmheiten";
+$l['celebritylist_nodata'] = "Es gibt noch keine eintragenen Berühmheiten.";
+$l['celebritylist_nodata_tab'] = "Es gibt noch keine eintragenen Berühmheiten für diese Branche.";
+
+$l['celebritylist_filter_button'] = "Berühmheiten filtern";
+$l['celebritylist_filter_type_headline'] = "nach Branche";
+$l['celebritylist_filter_type_firstselect'] = "alle Branchen";
+$l['celebritylist_filter_typePerson_headline'] = "nach Einzelpersonen oder Gruppen";
+$l['celebritylist_filter_typePerson_firstselect'] = "alle Berühmheiten";
+$l['celebritylist_filter_typePerson_single'] = "Einzelpersonen";
+$l['celebritylist_filter_typePerson_group'] = "Gruppen";
+
+$l['celebritylist_options_edit'] = "E";
+$l['celebritylist_options_delete'] = "X";
+$l['celebritylist_options_delete_notice'] = "Bist du dir sicher, dass du diesen Eintrag löschen möchtest?";
+
+$l['celebritylist_edit'] = "Berühmtheit '{1}' bearbeiten";
+
+$l['celebritylist_form'] = "Berühmheit eintragen";
+$l['celebritylist_form_button'] = "Abschicken";
+$l['celebritylist_form_character'] = "Charakter/Gruppe";
+$l['celebritylist_form_character_desc'] = "In die Liste können einzelne Personen eingetragen werden oder auch ganze Gruppen wie Bands oder Unternehmen, die eine wichtige Rolle spielen.";
+$l['celebritylist_form_profession'] = "Tätigkeit(en)";
+$l['celebritylist_form_profession_desc'] = "Als was ist der Charakter bzw. die Gruppe bekannt? Du kannst auch mehrer Tätigkeiten angeben - bitte trenne diese mit Kommas.";
+$l['celebritylist_form_profession_placeholder'] = "Musiker:in/Band, Schauspieler:in, Sportler:in, Unternehmen";
+$l['celebritylist_form_type'] = "Branche";
+$l['celebritylist_form_type_desc'] = "In welche Branche kann man den Charakter bzw. die Gruppe einsortieren?";
+$l['celebritylist_form_description'] = "Beschreibung";
+$l['celebritylist_form_description_desc'] = "Beschreibe den Charakter bzw. die Gruppe und für was diese(r) berühmt ist ausführlicher. Orientiere dich an den ersten Absatz eines Wikipedia-Artikels. Bei einer Gruppen können den einzelnen Mitglieder aufgelistet werden.";
+$l['celebritylist_form_button'] = "Abschicken";
+
+$l['celebritylist_error_character'] = "Es muss ein Charakter angegeben werden.";
+$l['celebritylist_error_group'] = "Es muss ein Name für die Gruppe angegeben werden.";
+$l['celebritylist_error_profession'] = "Es muss mindestens eine Tätigkeit angeben werden.";
+$l['celebritylist_error_type'] = "Es muss eine Branche ausgewählt werden.";
+$l['celebritylist_error_type_multi'] = "Es muss mindestens eine Branche ausgewählt werden.";
+$l['celebritylist_error_description'] = "Es muss ein Beschreibung angegeben werden.";
+
+$l['celebritylist_modcp'] = "Neue Berühmheiten verwalten";
+$l['celebritylist_modcp_nav'] = "Neue Berühmheiten";
+$l['celebritylist_modcp_nodata'] = "Es liegen keine neuen Berühmheiten zum überprüfen vor.";
+$l['celebritylist_modcp_accepted'] = "Annehmen";
+$l['celebritylist_modcp_refuse'] = "Ablehnen";
+$l['celebritylist_modcp_refuse_notice'] = "Möchtest du diesen Eintrag wirklich ablehnen? Er wird dann endgültig gelöscht.";
+
+$l['celebritylist_redirect_add_team'] = "Die neue Berühmheit wurde erfolgreich zur Liste hinzugefügt.<br />Du wirst jetzt zur Übersicht aller Berühmheiten zurückgeleitet.";
+$l['celebritylist_redirect_add_user'] = "Vielen Dank, deine Berühmheit wurde erfolgreich hinzugefügt. Ein Teammitglied wird dein Eintrag noch überprüfen.<br />Du wirst jetzt zur Übersicht aller Berühmheiten zurückgeleitet.";
+$l['celebritylist_redirect_edit'] = "Die Berühmheit wurde erfolgreich bearbeitet.<br />Du wirst jetzt zur Übersicht aller Berühmheiten zurückgeleitet.";
+$l['celebritylist_redirect_delete'] = "Die Berühmheit wurde erfolgreich gelöscht.<br />Du wirst jetzt zur Übersicht aller Berühmheiten zurückgeleitet.";
+$l['celebritylist_redirect_refuse'] = "Die Berühmheit wurde erfolgreich abgelehnt.<br />Du wirst jetzt zur Übersicht neuer Berühmheiten im ModCP zurückgeleitet.";
+$l['celebritylist_redirect_accepted'] = "Die Berühmheit wurde erfolgreich angenommen.<br />Du wirst jetzt zur Übersicht neuer Berühmheiten im ModCP zurückgeleitet.";
+
+$l['celebritylist_online_location'] = "Sieht sich die <a href=\"misc.php?action=celebritylist\">Übersicht aller Berühmheiten</a> an.";
+
+$l['celebritylist_alert_accepted'] = "{1} hat deinen Eintrag <b>{2}</b> als Berühmheit angenommen.";
+$l['myalerts_setting_celebritylist_accepted'] = "Benachrichtigungen erhalten, wenn deine eingetragene Berühmheiten angenommen wurde?";
+$l['celebritylist_alert_refuse'] = "{1} hat deinen Eintrag <b>{2}</b> als Berühmheit abgelehnt.";
+$l['myalerts_setting_celebritylist_refuse'] = "Benachrichtigungen erhalten, wenn deine eingetragene Berühmheiten abgelehnt wurde?";
